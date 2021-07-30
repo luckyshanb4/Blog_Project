8b6e09a82649b3a81d332d2fa7f922a24d02ecbe
luckyshanb4 <luckyshanb4@gmail.com>
2021-07-31 00:39:41 +0530

    added mongo db (locally)

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ const lodash=require("lodash");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const blogPostSchema=new mongoose.Schema({
+  title:String,
+  body:String
+})
+
+
+const Post=mongoose.model("Post",blogPostSchema);
+
 
 const app = express();
 
-const posts=[];
+// const posts=[];
 
 app.set('view engine', 'ejs');
 
@@ -20,10 +31,14 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-  
-  res.render('home',{startingContent:homeStartingContent,postsSent:posts});
 
+  Post.find(function(err,posts){
+    if(!err){
+      res.render('home',{startingContent:homeStartingContent,postsSent:posts});
+    }
+  })
   
+
 })
 
 
@@ -42,31 +57,57 @@ app.get('/compose',function(req,res){
 
 app.post('/compose',function(req,res){
   
-  const post={
-    title:req.body.postTitle,
-    content:req.body.postBody
-  }
+  let currentTitle=lodash.lowerCase(req.body.postTitle);
 
-  posts.push(post);
+  const post=new Post({
+    title:currentTitle,
+    body:req.body.postBody
+  })
+
+  post.save();
+
+  // const post={
+  //   title:req.body.postTitle,
+  //   content:req.body.postBody
+  // }
+
+  // posts.push(post);
   
   
   res.redirect("/");
   
 })
 
-app.get('/post/:postName',function(req,res){
-  let searchPostName=lodash.lowerCase(req.params.postName);
+app.get('/post/:postId',function(req,res){
+  let searchPostId=req.params.postId;
 
-  posts.forEach(function(post){
-    let currentTitle=lodash.lowerCase(post.title);
-
-    if(searchPostName===currentTitle){
-      console.log("Match Found!");
-      res.render('post',{postTitle:post.title,postContent:post.content});
+  
+  Post.findOne({_id:searchPostId},function(err,post){
+    if(!err){
+      if(post){
+        console.log("Match Found!");
+        res.render('post',{postTitle:post.title,postContent:post.body});
+      }
+      else{
+        console.log("Match Not Found!");
+      }
     }
-
-    console.log(req.params);
   })
+
+
+
+
+
+  // posts.forEach(function(post){
+  //   let currentTitle=lodash.lowerCase(post.title);
+
+  //   if(searchPostName===currentTitle){
+  //     console.log("Match Found!");
+  //     res.render('post',{postTitle:post.title,postContent:post.content});
+  //   }
+
+  //   console.log(req.params);
+  // })
   
 })
 
